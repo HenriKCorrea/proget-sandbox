f6eea9b168f6e20e64d4486648e0fa9bd8c675a4
Henrique Krausburg Correa <hcorrea@ael.com.br>
2025-01-24 15:56:54 -0300

    updating create-database.sh script

--- scripts/create-database.sh
+++ scripts/create-database.sh
@@ -7,18 +7,21 @@ pushd "$(dirname "$0")"
 source ../.env
 
 # Array of database names
-databases=("ProGet" "BuildMaster" "Otter")
+databases=()
+databases+=("ProGet")
+# databases+=("BuildMaster")
+# databases+=("Otter")
 
 for db in "${databases[@]}"; do
   # Check if database exists
-  DB_EXISTS=$(docker exec inedo-sql /opt/mssql-tools/bin/sqlcmd \
-    -S localhost -U SA -P ${MSSQL_SA_PASSWORD} \
+  DB_EXISTS=$(podman.exe exec inedo-sql /opt/mssql-tools18/bin/sqlcmd \
+    -C -S localhost -U SA -P ${MSSQL_SA_PASSWORD} \
     -Q 'SELECT name FROM sys.databases' | grep -w "$db")
 
   if [ -z "$DB_EXISTS" ]; then
     echo "Creating $db database..."
-    docker exec inedo-sql /opt/mssql-tools/bin/sqlcmd \
-      -S localhost -U SA -P ${MSSQL_SA_PASSWORD} \
+    podman.exe exec inedo-sql /opt/mssql-tools18/bin/sqlcmd \
+      -C -S localhost -U SA -P ${MSSQL_SA_PASSWORD} \
       -Q "CREATE DATABASE [$db] COLLATE SQL_Latin1_General_CP1_CI_AS"
   else
     echo "$db database already exists!"
